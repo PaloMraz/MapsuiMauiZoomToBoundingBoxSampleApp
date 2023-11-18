cbab2a9382ec146cec035ee2ef200b589eb1d1ff
Palo Mraz <pavol.mraz@ftg.sk>
2023-11-18 08:19:54 +0100

    Added some comments, renamed screenshots.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,10 +9,13 @@ namespace MapsuiMauiZoomToBoundingBoxSampleApp
 {
   public partial class MainPage : ContentPage
   {
+    // The two locations I want to zoom in on the map.
     private readonly Location _devinCastleGpsLocation = new Location(48.17442695182697, 16.97759876457116);
     private readonly (double X, double Y) _devinCastleMercatorLocation;
     private readonly Location _bratislavaCastleGpsLocation = new Location(48.142273232469634, 17.100320628008593);
     private readonly (double X, double Y) _bratislavaCastleMercatorLocation;
+
+
     public MainPage()
     {
       InitializeComponent();
@@ -21,9 +24,11 @@ namespace MapsuiMauiZoomToBoundingBoxSampleApp
         $"Zoom to {this._devinCastleGpsLocation.Latitude},{this._devinCastleGpsLocation.Longitude} - " + 
         $"{this._bratislavaCastleGpsLocation.Latitude}, {this._bratislavaCastleGpsLocation.Longitude}";
 
+      // Convert GPS to Mercator projection.
       this._devinCastleMercatorLocation = SphericalMercator.FromLonLat(this._devinCastleGpsLocation.Longitude, this._devinCastleGpsLocation.Latitude);
       this._bratislavaCastleMercatorLocation = SphericalMercator.FromLonLat(this._bratislavaCastleGpsLocation.Longitude, this._bratislavaCastleGpsLocation.Latitude);
 
+      // Setup the map and location pins.
       this._mapControl.Map.Layers.Add(OpenStreetMap.CreateTileLayer());
       var pinsLayer = new GenericCollectionLayer<List<IFeature>>
       {
@@ -43,13 +48,17 @@ namespace MapsuiMauiZoomToBoundingBoxSampleApp
 
     private void _zoomToBoundsButton_Clicked(object sender, EventArgs e)
     {
+      // Compute the bounding box containing the two locations.
       double minX = Math.Min(this._devinCastleMercatorLocation.X, this._devinCastleMercatorLocation.X);
       double maxX = Math.Max(this._bratislavaCastleMercatorLocation.X, this._bratislavaCastleMercatorLocation.X);
       double minY = Math.Min(_devinCastleMercatorLocation.Y, _devinCastleMercatorLocation.Y);
       double maxY = Math.Max(_bratislavaCastleMercatorLocation.Y, _bratislavaCastleMercatorLocation.Y);
-      var bounds = new MRect(minX, minY, maxX, maxY).Grow(500);
-      this._mapControl.Map.Navigator.ZoomToBox(box: bounds, boxFit: MBoxFit.Fill);
 
+      // Grow it by 500 meters to ensure the pins are visible.
+      var bounds = new MRect(minX, minY, maxX, maxY).Grow(500);
+
+      // Zoom the map to the bounds.
+      this._mapControl.Map.Navigator.ZoomToBox(box: bounds, boxFit: MBoxFit.Fill);
     }
   }
 
